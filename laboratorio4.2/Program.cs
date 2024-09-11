9cbf8f97179724cdae442fa7675b8c43949f44e2
RariasL <rodolfo.arias@utp.ac.pa>
2024-09-11 12:00:44 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio4.2/Program.cs
@@ -0,0 +1,14 @@
+﻿static void Main(string[] args)
+{
+    int fac = 1, n;
+    string linea;
+    Console.WriteLine("ingrese un numero entero:");
+    linea = Console.ReadLine();
+    n = int.Parse(linea);
+    for(int i = 1; 1 <= n; i++)
+    {
+        fac = fac * 1;
+    }
+    Console.WriteLine("la factorial es: " + fac);
+    Console.ReadKey();
+}
---
+++ laboratorio4.6/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digite el numero del radio del circulo");
+        double radio = double.Parse(Console.ReadLine());
+
+        double area = Math.Pow(radio, 2) * Math.PI;
+    }
+}
